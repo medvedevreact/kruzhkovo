03606bc8e23e12bd08c277893f4cdd52b95c0a2e
fullstakilla <selakovich@mail.ru>
2026-06-07 21:58:27 +0300

    add: images unoptimized

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',  // Включает статический экспорт
   distDir: 'build',  // Меняем имя папки на 'build'
+  images: {
+    unoptimized: true, // отключает API оптимизации
+  }
 };
 
 export default nextConfig;
